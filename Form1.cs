1d3ad95c19698c0ae3d275ea9f861097719800eb
djemjan58 <64644157+djemjan58@users.noreply.github.com>
2020-05-31 13:03:03 +0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication9
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.URL = "http://46.20.3.201:80/";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.URL = "http://46.20.3.246/stream/507/";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.URL = "http://n10101m.mediatriple.net/videoonlylive/mtkgeuihrlfwlive/u_stream_5c9e2f95dcb16_1/playlist.m3u8";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.URL = "http://45.32.154.169:9300/;";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.URL = "http://sunucu.radyodinle.com:8150";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.URL = "http://46.20.3.230";
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.URL = "http://windows.showradyo.com.tr/;";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.URL = "http://yayin.damarfm.com:8080/mp3";
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.URL = "http://46.20.3.207/";
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.URL = "http://37.247.98.8/stream/30/";
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        
+    }
+}
